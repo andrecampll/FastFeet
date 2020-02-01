48992e357a2c961057f6d640dfd9f64ff43b040d
André Victor <andrevictor50@gmail.com>
2020-02-01 14:44:55 -0300

    First Commit to the api

---
+++ api-node/src/routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get(
+  ('/',
+  (req, res) => {
+    return res.json({ message: 'alo' });
+  })
+);
+
+export default routes;
